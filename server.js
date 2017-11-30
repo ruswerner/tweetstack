0d93e24ab5ffc96b9555561b1db3a6255dbd69ec
ruswerner <rus.werner@gmail.com>
2017-12-01 10:44:58 +1300

    adding static assets; fix up static serving on server

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 
+const path = require('path');
 const App = require('./server/app');
 const twitter = require('./server/lib/twitter');
 
 const app = App(twitter);
 
 const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Serving assets from `client/build`...');
+  const express = require('express');
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server available at http://localhost:${PORT}/`);
--- server/app.js
+++ server/app.js
@@ -5,10 +5,6 @@ module.exports = twitter => {
 
   const app = express();
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  }
-
   app.get('/api/search', searchController(twitter));
 
   return app;
